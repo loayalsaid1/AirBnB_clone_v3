7a6e35930807392c83beb46b1917a59ed27b90d7
Loay Al-Said <l.programming555@gmail.com>
2024-05-02 04:01:51 +0300

    /api/v1/stats endpoint; Count of objects/class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,36 @@
 """Views.index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
+}
 
 
 @app_views.route("/status")
 def status():
     """Status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Return number of objects for each veiw|class"""
+    stats = {}
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+
+    return jsonify(stats)
