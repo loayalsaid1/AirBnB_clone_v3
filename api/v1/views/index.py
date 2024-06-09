9a0341fcbe79d59f505503b46ec9cd0f90d96677
Innocent Anozie <iygeal@gmail.com>
2024-06-09 17:50:30 +0100

    wait

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -7,3 +15,19 @@ from flask import jsonify
 def status():
     """Status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+
+    stats = {key: storage.count(cls) for key, cls in classes.items()}
+    return jsonify(stats)
