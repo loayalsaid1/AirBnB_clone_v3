f93c8a593692683cc4b63595d0ffdcb6e58a486f
Loay Al-Said <l.programming555@gmail.com>
2024-06-10 02:05:12 +0300

    add 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The core of the api"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -12,6 +12,13 @@ app.register_blueprint(app_views)
 def close(exception):
 	"""Close the session after each request"""
 	storage.close()
+
+
+@app.errorhandler(404)
+def not_fount_error(e):
+	"""Return error message"""
+	return jsonify({"error": "Not found"}), 404
+
 
 
 if __name__ == "__main__":
