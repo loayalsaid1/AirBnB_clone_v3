566b2d170d006dca52dfd6580c45bed56e71dbfb
Innocent Anozie <iygeal@gmail.com>
2024-06-09 17:50:30 +0100

    Done with task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The core of the api"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -8,15 +8,22 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(exception):
-	"""Close the session after each request"""
-	storage.close()
+    """Close the session after each request"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors by returning a JSON-formatted response."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-	from os import getenv
-	host = getenv("HBNB_API_HOST", "0.0.0.0")
-	port = getenv("HBNB_API_PORT", "5000")
+    from os import getenv
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
 
-	app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
