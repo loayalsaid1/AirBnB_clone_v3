a7e7640860571a22d9ce4a8d7863e0fd46049049
Innocent Anozie <iygeal@gmail.com>
2024-06-10 15:34:17 +0100

    Created aemnities.py to handle routes for the Amenity objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+<<<<<<< HEAD
+=======
+from api.v1.views.amenities import *
+>>>>>>> 6fe33a8 (Created aemnities.py to handle routes for the Amenity objects)
